22ef42c1cdbd206b009a9b24a945fd519c1e0e9e
Omer <omrebres@gmail.com>
2020-08-27 22:18:45 +0300

    Project 2

---
+++ Assets/Scripts/DoorMotion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMotion : MonoBehaviour
+{
+    public Animator animator;
+    public bool isOpen;
+
+    void Start()
+    {
+        animator = GameObject.FindWithTag("doorAxis").GetComponent<Animator>();
+        isOpen = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger enter");
+        if (other.CompareTag("MainCamera"))
+        {
+            animator.SetTrigger("DoorOpen");
+            isOpen = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger exit");
+        if (isOpen)
+        {
+            animator.SetTrigger("DoorClose");
+            isOpen = false;
+        }
+    }
+}
